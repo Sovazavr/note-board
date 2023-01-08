46d1e6196751213787224d09d82daac364f6c422
sovazavr <vadimsopov00@mail.ru>
2023-01-08 07:36:20 +0700

    install sass & style upgrade

--- src/MenuComponent/MenuComponent.jsx
+++ src/MenuComponent/MenuComponent.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./MenuStyle.css"
+import "./MenuStyle.scss"
 import {useState} from "react";
 
 const MenuComponent = () => {
@@ -7,6 +7,11 @@ const [opened, setOpened]=useState(false)
   return (
     <div id="mySidenav" className={opened ? "MenuOpen" : "MenuClose"}>
         <button onClick={()=>setOpened(prev=>!prev)} className={opened ? "closeButton" : "openButton"}>&lt;</button>
+        <div className="textBlock">
+          <div>text 1</div>
+          <div>text 2</div>
+          <div>text 3</div>
+        </div>
     </div>
   )
 }
